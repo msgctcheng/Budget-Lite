95e56dbcbf571ef787e1eba3be4cdf986e1c5104
Philippe Dixon <pdixon555@hotmail.com>
2017-10-10 21:08:47 -0400

    add login route

--- controllers/budget-controllers.js
+++ controllers/budget-controllers.js
@@ -2,26 +2,13 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
-router.get("/", function(req, res) {
-    // db.Burger.findAll({})
-    //     .then(function(dbBurger) {
-    //         var burgerArray = [];
-
-    //         for (burg in dbBurger) {
-    //             burgerArray.push(dbBurger[burg].dataValues);
-    //         }
-
-    //         var handlebarsObj = {
-    //             burgers : burgerArray
-    //         }
-            // console.log("database: ", handlebarsObj);
+router.get("/", function(req, res) {    
             handlebarsObj = { budget : {"testing" : "works"}};
             res.render("index", handlebarsObj);
-        // });
-
-
 });
 
-
+router.get("/login", function(req, res) {
+	res.render("login");
+});
 
 module.exports = router;
