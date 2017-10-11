438f62efa85775b145ea34c1b35cf8734d723f24
Elizabeth Prieto <eprieto.business@gmail.com>
2017-10-10 21:35:15 -0400

    update

--- controllers/budget-controllers.js
+++ controllers/budget-controllers.js
@@ -11,4 +11,26 @@ router.get("/login", function(req, res) {
 	res.render("login");
 });
 
+router.get("/api/transactions", function(req, res) {
+	db.Transactions.findAll({})
+	.then(function(dbTrans) {
+	  res.json(dbTrans);
+	});
+});
+
+router.post("/api/transactions", function(req, res) {
+	db.Transactions.create(req.body)
+	.then(function(dbTrans) {
+		res.json(dbTrans);
+	});
+});
+
+router.get("/api/user", function(req, res) {
+	db.User.findAll({
+		include: [db.Transactions]
+	}).then(function(dbUser) {
+	  res.json(dbUser);
+	});
+});  
+
 module.exports = router;
