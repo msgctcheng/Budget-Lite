afb34885ffa2e1cf671b9c39f5e08119fb59e5b8
msgctcheng <msgctcheng@gmail.com>
2017-10-12 19:28:01 -0400

    Fix file load error

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.use(express.static("public"));
+
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
