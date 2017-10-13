80f210acdb0f4ffd9200b397f461e3303484bbd8
Elizabeth Prieto <eprieto.business@gmail.com>
2017-10-12 20:42:52 -0400

    "takes google id input"

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // Giving the User model a name of type STRING
+    googleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [1]
+    },
     name: {
       type: DataTypes.STRING, 
       allowNull: false,
